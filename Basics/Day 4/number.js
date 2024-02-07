b28ac11108429c03ca1a708918161b5ef7e198c5
unknown <sohailshaikh2379@gmail.com>
2024-02-07 21:51:07 +0530

    Test

--- Basics/Day 4/number.js
+++ Basics/Day 4/number.js
@@ -1,2 +1,21 @@
 let num = 15;
 console.log(num);
+
+let arr = [1,4,2,3,5,4];
+let repeatNum = arr.filter((val,index,self) =>{
+    return self.indexOf(val) !== index;
+})
+
+console.log(arr.sort())
+console.log("Largest : ", arr.length -0);
+console.log("Secound Largest : ", arr.length -1);
+console.log("Repeating : " , repeatNum);
+
+
+let fruit = ["apple","Banana", "Orange", "apple"];
+
+let repeatFruit = fruit.filter((val,index,self) => {
+    return self.indexOf(val) !== index;
+})
+console.log(fruit);
+console.log(repeatFruit);
