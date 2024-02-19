0f960849541f4da89ed3b1bfe468e89a8594aaab
unknown <sohailshaikh2379@gmail.com>
2024-02-19 22:19:54 +0530

    practice functions

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,7 +1,34 @@
 function sayMyName() {
     console.log("Hello World");
-    return "sohail"
+    console.log('S');
+    console.log('O');
+    console.log('H');
+    console.log('A');
+    console.log('I');
+    console.log('L');
 }
 
-
 sayMyName();
+
+function addTwoNumber (a,b) {
+    console.log(a+b);
+}
+// console.log(addTwoNumber(10,5));
+addTwoNumber(5,10);
+
+let myName = () => {
+    return "Sohail"
+} 
+console.log(myName());
+
+let loginUser = (username) => {
+    return `${username} just logged in `
+}
+
+console.log(loginUser("Sohail"));
+
+function myEmail (email) {
+    return `${email} is correct`
+}
+
+console.log(myEmail("SohailShaikh@2379@live.com"));
