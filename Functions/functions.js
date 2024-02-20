43fc8f837be679db0d92b67ccc7ed16fee63cbb1
unknown <sohailshaikh2379@gmail.com>
2024-02-20 21:30:12 +0530

    If_Else_Statement

--- Functions/functions.js
+++ Functions/functions.js
@@ -8,27 +8,37 @@ function sayMyName() {
     console.log('L');
 }
 
-sayMyName();
+//sayMyName();
 
 function addTwoNumber (a,b) {
     console.log(a+b);
 }
 // console.log(addTwoNumber(10,5));
-addTwoNumber(5,10);
+//addTwoNumber(5,10);
 
 let myName = () => {
     return "Sohail"
 } 
-console.log(myName());
+//console.log(myName());
 
 let loginUser = (username) => {
     return `${username} just logged in `
 }
 
-console.log(loginUser("Sohail"));
+// console.log(loginUser("Sohail"));
 
 function myEmail (email) {
     return `${email} is correct`
 }
 
-console.log(myEmail("SohailShaikh@2379@live.com"));
+// console.log(myEmail("SohailShaikh@2379@live.com"));
+
+let getClarity = () => {
+    if (getClarity === undefined) {
+        console.log("Sohail don't get the clarity");
+    } else {
+        console.log("He got the clarity");
+    }
+}
+
+console.log(getClarity("Yess"));
