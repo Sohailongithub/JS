aeaccbcc8fa0e19a6e1a8fd0c6ce3cda3295ddd3
unknown <sohailshaikh2379@gmail.com>
2024-02-24 20:38:47 +0530

    Hoisting Practice

---
+++ Hoisting/Scope/hoisting.js
@@ -0,0 +1,13 @@
+function one () {
+    const username = 'soahil'
+
+    function two () {
+        const website = 'sphinix.com'
+         console.log(username);
+         console.log(website);
+    }
+    //  console.log(website);
+    // two();
+}
+
+// one ();
