506ab86628adfff3945e4accf1937a869e100c94
unknown <sohailshaikh2379@gmail.com>
2024-04-19 16:38:05 +0530

    Sample Project

---
+++ Module 2/Control Flow/whileLoop.js
@@ -0,0 +1,35 @@
+// While Loop
+
+// while (condition) {
+    
+// }
+
+let odd = 1
+while (odd <= 10) {
+    console.log(`Odd value  is ${odd}`);
+    odd = odd + 2
+}
+
+let even = 0
+while (even < 10) {
+    console.log(`Even Value is ${even}`);
+    even = even + 2
+}
+
+let carModel = ['Honda', 'Kia', 'MG'];
+
+let index = 0;
+
+while (index < carModel.length) {
+    console.log(`Car Model is ${carModel[index]}`);
+    index++
+}
+
+let bikeModel = ['hero', 'KTM', 'TVS'];
+let i = 0;
+
+while (i < bikeModel.length) {
+    console.log(`Bike Model of ${bikeModel[i]}`);
+    i++
+}
+
